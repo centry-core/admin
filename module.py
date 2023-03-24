e89e704a57f7d4558db5a22def1e0704cd34050d
Aspect13 <ad13box@gmail.com>
2023-03-24 13:12:31 +0400

    secrets, artifacts and api changes for modes

--- module.py
+++ module.py
@@ -16,18 +16,11 @@
 #   limitations under the License.
 
 """ Module """
-
-# import sqlalchemy  # pylint: disable=E0401
-
 from pylon.core.tools import log  # pylint: disable=E0401
 from pylon.core.tools import module  # pylint: disable=E0401
-from pylon.core.tools.context import Context as Holder  # pylint: disable=E0401
 
-from tools import theme  # pylint: disable=E0401
-from tools import db  # pylint: disable=E0401
-from tools import db_migrations  # pylint: disable=E0401
-
-# from .db import init_db
+from tools import theme, VaultClient  # pylint: disable=E0401
+import hvac
 
 
 class Module(module.ModuleModel):
@@ -109,8 +102,22 @@ class Module(module.ModuleModel):
             permissions=["global_admin"],
             prefix="admin_mode_projects_edit_",
         )
+
+        theme.register_mode_section(
+            "administration", "configuration", "Configuration",
+            kind="holder",
+            location="left",
+            permissions=["global_admin"],
+            # icon_class="fas fa-info-circle fa-fw",
+        )
         # Init
         self.descriptor.init_all()
+
+        vc = VaultClient()
+        try:
+            vc.init_project_space()
+        except hvac.exceptions.InvalidRequest:
+            ...
 
     def deinit(self):  # pylint: disable=R0201
         """ De-init module """
--- static/js/projects.js
+++ static/js/projects.js
@@ -1,19 +1,34 @@
-function projectCreateSubmit() {
-    fetch('/api/v1/projects/project/', {
+const set_errors = error_data => {
+    error_data?.loc.forEach(i => {
+        const inpt = $(`#${i}`)
+        inpt.addClass('is-invalid')
+        inpt.siblings('.invalid-feedback').text(error_data?.msg)
+    })
+}
+
+const clear_errors = () => {
+    ['project_name', 'project_admin_email'].forEach(i => {
+        $(`#${i}`).removeClass('is-invalid')
+    })
+}
+
+async function projectCreateSubmit() {
+    clear_errors()
+    const resp = await fetch('/api/v1/projects/project/', {
         method: 'POST',
         headers: {'Content-Type': 'application/json'},
         body: JSON.stringify({
             "name": $('#project_name').val(),
-            "owner": "",
+            "project_admin_email": $('#project_admin_email').val(),
             "vuh_limit": 60000,
             "plugins": vueVm.multiselectFilter?.selectedItems.map(i => i.id) || [],
             "storage_space_limit": 1000000000,
             "data_retention_limit": 1000000000,
-            "invitations": []
         })
-    }).then(async response => {
+    })
+    if (resp.ok) {
         $("#projectCreateModal").modal('hide');
-        const data = await response.json()
+        const data = await resp.json()
         const newSections = data['plugins']
         const currentSection = "admin"
         const notAdmin = !vueVm.navbar.isAdmin
@@ -21,13 +36,15 @@ function projectCreateSubmit() {
         if (notAdmin && !newSections.includes(currentSection) && newSections.length > 0) {
             const section = newSections[0]
             location.href = `/-/${section}`
-            $('select.selectpicker[name=`section_select`]').val(section)
+            $('select.selectpicker[name="section_select"]').val(section)
         } else {
             location.reload()
         }
-    })
+    } else {
+        const err_data = await resp.json()
+        set_errors(err_data)
+    }
 }
-
 
 function projectActionFormatter(value, row, index) {
     return '<a href="/~/administration/~/projects/list/edit?project=' + row.id + '"><i class="fas fa-sync"></i></a>'
