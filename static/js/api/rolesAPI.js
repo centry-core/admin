d0453f00dd85db2b25474bf93dacec4fa9cd0210
Dmitrii_Belov1 <Dmitrii_Belov1@epam.com>
2023-04-05 13:54:35 +0600

    Prototype of roles

--- static/js/api/rolesAPI.js
+++ static/js/api/rolesAPI.js
@@ -1,50 +1,65 @@
-const fetchPermissionsAPI = (selectedMode) => {
-    return fetch(`/api/v1/admin/permissions/${selectedMode}`).then((data) => {
+const fetchPermissionsAPI = selectedMode => {
+    const api_url = V.build_api_url('admin', 'permissions')
+    return fetch(
+        `${api_url}/${(V.mode === 'administration' ? `${selectedMode}` : `${V.project_id}`)}/`
+    ).then((data) => {
         return data.json()
     })
 }
 const fetchRolesAPI = (selectedMode) => {
-    return fetch(`/api/v1/admin/roles/${selectedMode}`).then((data) => {
+    const api_url = V.build_api_url('admin', 'roles')
+    return fetch(
+        `${api_url}/${(V.mode === 'administration' ? `${selectedMode}` : `${V.project_id}`)}/`
+    ).then((data) => {
         return data.json()
     })
 }
 const saveRolesPermissionsAPI = async (tableData, selectedMode) => {
-    const res = await fetch(`/api/v1/admin/permissions/${selectedMode}`, {
-        method: 'PUT',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(tableData)
-    })
+    const api_url = V.build_api_url('admin', 'permissions')
+    const res = await fetch(
+        `${api_url}/${(V.mode === 'administration' ? `${selectedMode}` : `${V.project_id}`)}/`, {
+            method: 'PUT',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(tableData)
+        })
     return res.json();
 }
 const createRoleAPI = async (newRoleName, selectedMode) => {
-    const res = await fetch(`/api/v1/admin/roles/${selectedMode}`, {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({name: newRoleName})
-    })
+    const api_url = V.build_api_url('admin', 'roles')
+    const res = await fetch(
+        `${api_url}/${(V.mode === 'administration' ? `${selectedMode}` : `${V.project_id}`)}/`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({name: newRoleName})
+        })
     return res.json();
 }
 const deleteRoleAPI = async (roleName, selectedMode) => {
-    const res = await fetch(`/api/v1/admin/roles/${selectedMode}`, {
-        method: 'DELETE',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({name: roleName})
-    })
+    const api_url = V.build_api_url('admin', 'roles')
+    const res = await fetch(
+        `${api_url}/${(V.mode === 'administration' ? `${selectedMode}` : `${V.project_id}`)}/`
+        , {
+            method: 'DELETE',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({name: roleName})
+        })
     return res.json();
 }
 const updateRoleNameAPI = async (oldRoleName, newRoleName, selectedMode) => {
-    const res = await fetch(`/api/v1/admin/roles/${selectedMode}`, {
-        method: 'PUT',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({name: oldRoleName, new_name: newRoleName})
-    })
+    const api_url = V.build_api_url('admin', 'roles')
+    const res = await fetch(
+        `${api_url}/${(V.mode === 'administration' ? `${selectedMode}` : `${V.project_id}`)}/`, {
+            method: 'PUT',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({name: oldRoleName, new_name: newRoleName})
+        })
     return res.json();
 }
