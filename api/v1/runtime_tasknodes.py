9ff37cafb62d104ddb37ee936e91b51a658514fb
Ivan Krakhmaliuk <Ivan_Krakhmaliuk@epam.com>
2024-11-15 19:21:27 +0200

    ability to request node state sync

--- api/v1/runtime_tasknodes.py
+++ api/v1/runtime_tasknodes.py
@@ -29,56 +29,137 @@ class AdminAPI(api_tools.APIModeHandler):  # pylint: disable=R0903
     """ API """
 
     @auth.decorators.check_api(["runtime.plugins"])
-    def get(self):
+    def get(self):  # pylint: disable=R0911,R0912
         """ Process GET """
-        result = []
-        #
+        action = flask.request.args.get("action", "list")
         node = flask.request.args.get("node", None)
         scope = flask.request.args.get("scope", None)
         #
         module_manager = self.module.context.module_manager
         #
-        if node is not None and scope is not None:
+        # list
+        #
+        if action == "list":
+            result = []
+            #
+            if node is not None and scope is not None:
+                plugin_name, task_node_name = node.split(".", 1)
+                #
+                if plugin_name in module_manager.modules:
+                    plugin = module_manager.modules[plugin_name].module
+                    task_node = getattr(plugin, task_node_name, None)
+                    #
+                    if task_node is not None:
+                        if scope == "pool":
+                            with task_node.lock:
+                                for state in task_node.global_pool_state.values():
+                                    for node in state.values():
+                                        result.append({
+                                            "pool": node.get("pool", None),
+                                            "ident": node.get("ident", None),
+                                            "task_limit": node.get("task_limit", None),
+                                            "running_tasks": node.get("running_tasks", None),
+                                        })
+                        #
+                        if scope == "task":
+                            with task_node.lock:
+                                for state in task_node.global_task_state.values():
+                                    meta = state.get("meta", None)
+                                    if meta is not None:
+                                        meta = str(meta)
+                                    #
+                                    status = state.get("status", None)
+                                    if status == "stopped":
+                                        continue
+                                    #
+                                    result.append({
+                                        "task_id": state.get("task_id", None),
+                                        "requestor": state.get("requestor", None),
+                                        "runner": state.get("runner", None),
+                                        "status": status,
+                                        "meta": meta,
+                                    })
+            #
+            return {
+                "total": len(result),
+                "rows": result,
+            }
+        #
+        # refresh
+        #
+        if action == "refresh":
+            if node is None or scope is None:
+                return {
+                    "ok": False,
+                    "error": "node or scope not set",
+                }
+            #
             plugin_name, task_node_name = node.split(".", 1)
             #
-            if plugin_name in module_manager.modules:
-                plugin = module_manager.modules[plugin_name].module
-                task_node = getattr(plugin, task_node_name, None)
-                #
-                if task_node is not None:
-                    if scope == "pool":
-                        with task_node.lock:
-                            for state in task_node.global_pool_state.values():
-                                for node in state.values():
-                                    result.append({
-                                        "pool": node.get("pool", None),
-                                        "ident": node.get("ident", None),
-                                        "task_limit": node.get("task_limit", None),
-                                        "running_tasks": node.get("running_tasks", None),
-                                    })
-                    #
-                    if scope == "task":
-                        with task_node.lock:
-                            for state in task_node.global_task_state.values():
-                                meta = state.get("meta", None)
-                                if meta is not None:
-                                    meta = str(meta)
-                                #
-                                status = state.get("status", None)
-                                if status == "stopped":
-                                    continue
-                                #
-                                result.append({
-                                    "task_id": state.get("task_id", None),
-                                    "requestor": state.get("requestor", None),
-                                    "runner": state.get("runner", None),
-                                    "status": status,
-                                    "meta": meta,
-                                })
+            if plugin_name not in module_manager.modules:
+                return {
+                    "ok": False,
+                    "error": "unknown plugin",
+                }
+            #
+            plugin = module_manager.modules[plugin_name].module
+            task_node = getattr(plugin, task_node_name, None)
+            #
+            if task_node is None:
+                return {
+                    "ok": False,
+                    "error": "unknown node",
+                }
+            #
+            if scope == "pool":
+                task_node.query_pool_state()
+            elif scope == "task":
+                task_node.query_task_state()
+            else:
+                return {
+                    "ok": False,
+                    "error": "unknown scope",
+                }
+            #
+            return {
+                "ok": True,
+            }
+        #
+        # stop
+        #
+        if action == "stop":
+            if node is None or scope is None:
+                return {
+                    "ok": False,
+                    "error": "node or scope not set",
+                }
+            #
+            plugin_name, task_node_name = node.split(".", 1)
+            #
+            if plugin_name not in module_manager.modules:
+                return {
+                    "ok": False,
+                    "error": "unknown plugin",
+                }
+            #
+            plugin = module_manager.modules[plugin_name].module
+            task_node = getattr(plugin, task_node_name, None)
+            #
+            if task_node is None:
+                return {
+                    "ok": False,
+                    "error": "unknown node",
+                }
+            #
+            task_node.stop_task(scope)
+            #
+            return {
+                "ok": True,
+            }
         #
         return {
-            "total": len(result),
-            "rows": result,
+            "ok": False,
+            "error": "unknown action",
         }
 
 
--- static/js/runtime_tasknodes.js
+++ static/js/runtime_tasknodes.js
@@ -1,3 +1,35 @@
 $(".refresh-table-button").click(function() {
   $($(this).data("target")).bootstrapTable("refresh", {});
 });
+
+$(".refresh-pool-button").click(function() {
+  axios.get(tasknodes_api_url, {params: {action: "refresh", node: $(this).data("node"), scope: "pool"}})
+    .then(function (response) {
+      if (response.data.ok) {
+        showNotify("SUCCESS", "Refresh request completed");
+        $($(this).data("target")).bootstrapTable("refresh", {});
+      } else {
+        showNotify("ERROR", response.data.error);
+      }
+    })
+    .catch(function (error) {
+      showNotify("ERROR", "Error during refresh request");
+      console.log(error);
+    });
+});
+
+$(".refresh-task-button").click(function() {
+  axios.get(tasknodes_api_url, {params: {action: "refresh", node: $(this).data("node"), scope: "task"}})
+    .then(function (response) {
+      if (response.data.ok) {
+        showNotify("SUCCESS", "Refresh request completed");
+        $($(this).data("target")).bootstrapTable("refresh", {});
+      } else {
+        showNotify("ERROR", response.data.error);
+      }
+    })
+    .catch(function (error) {
+      showNotify("ERROR", "Error during refresh request");
+      console.log(error);
+    });
+});
