7bfa33ef3426260e0d71f2da14cfbe7a74a560ab
Ivan Krakhmaliuk <Ivan_Krakhmaliuk@epam.com>
2024-11-15 19:25:24 +0200

    fix refresh

--- static/js/runtime_tasknodes.js
+++ static/js/runtime_tasknodes.js
@@ -3,11 +3,13 @@ $(".refresh-table-button").click(function() {
 });
 
 $(".refresh-pool-button").click(function() {
+  var target = $(this).data("target");
+  //
   axios.get(tasknodes_api_url, {params: {action: "refresh", node: $(this).data("node"), scope: "pool"}})
     .then(function (response) {
       if (response.data.ok) {
         showNotify("SUCCESS", "Refresh request completed");
-        $($(this).data("target")).bootstrapTable("refresh", {});
+        $(target).bootstrapTable("refresh", {});
       } else {
         showNotify("ERROR", response.data.error);
       }
@@ -19,11 +21,13 @@ $(".refresh-pool-button").click(function() {
 });
 
 $(".refresh-task-button").click(function() {
+  var target = $(this).data("target");
+  //
   axios.get(tasknodes_api_url, {params: {action: "refresh", node: $(this).data("node"), scope: "task"}})
     .then(function (response) {
       if (response.data.ok) {
         showNotify("SUCCESS", "Refresh request completed");
-        $($(this).data("target")).bootstrapTable("refresh", {});
+        $(target).bootstrapTable("refresh", {});
       } else {
         showNotify("ERROR", response.data.error);
       }
