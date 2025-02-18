02915f77eca20261afa58c2ab7ba4cba6cdf6036
Ivan Krakhmaliuk <lifedj27@gmail.com>
2025-02-18 22:37:30 +0200

    tasknode task logs

--- static/js/runtime_tasknodes.js
+++ static/js/runtime_tasknodes.js
@@ -41,8 +41,37 @@ $(".refresh-task-button").click(function() {
 });
 
 
+$("#modal-task-logs").on("show.bs.modal", function (e) {
+  $("#modal-logs-task").text(logs_row.task_id);
+  $("#input-logs").val("");
+  //
+  window.socket.emit("task_logs_subscribe", {"tasknode_task": "id:" + logs_row.task_id});
+});
+
+
+$("#modal-task-logs").on("hide.bs.modal", function (e) {
+  $("#modal-logs-task").text("");
+  $("#input-logs").val("");
+  //
+  window.socket.emit("task_logs_unsubscribe", {"tasknode_task": "id:" + logs_row.task_id});
+});
+
+
+$(document).on("vue_init", () => {
+  window.socket.on("log_data", (data) => {
+    $("#input-logs").val(
+      $("#input-logs").val() + "\n" + data.line
+    );
+  });
+});
+
+
 function actions_formatter(value, row, index) {
   return [
+    '<a class="task-show-logs" href="javascript:void(0)" title="Show logs">',
+    '  <i class="fa fa-file-text" style="color: #858796"></i>',
+    '</a>',
+    '&nbsp;',
     '<a class="task-stop" href="javascript:void(0)" title="Stop">',
     '  <i class="fa fa-stop"></i>',
     '</a>',
@@ -64,5 +93,9 @@ window.actions_events = {
         showNotify("ERROR", "Error during stop request");
         console.log(error);
       });
+  },
+  "click .task-show-logs": function (e, value, row, index) {
+    logs_row = row;
+    $("#modal-task-logs").modal("show");
   }
 }
