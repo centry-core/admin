952758baa313324d0098582bff20980f168dde3b
Ivan Krakhmaliuk <Ivan_Krakhmaliuk@epam.com>
2025-03-10 20:13:13 +0200

    Tasks: WIP

---
+++ api/v1/tasks.py
@@ -0,0 +1,166 @@
+#!/usr/bin/python3
+# coding=utf-8
+
+#   Copyright 2025 getcarrier.io
+#
+#   Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS,
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#   See the License for the specific language governing permissions and
+#   limitations under the License.
+
+""" API """
+
+import flask  # pylint: disable=E0401,W0611
+
+from pylon.core.tools import log  # pylint: disable=E0611,E0401,W0611
+
+from tools import auth  # pylint: disable=E0401
+from tools import api_tools  # pylint: disable=E0401
+
+
+class AdminAPI(api_tools.APIModeHandler):  # pylint: disable=R0903
+    """ API """
+
+    @auth.decorators.check_api(["runtime.plugins"])
+    def get(self):  # pylint: disable=R0911,R0912
+        """ Process GET """
+        action = flask.request.args.get("action", "list")
+        scope = flask.request.args.get("scope", None)
+        #
+        module_manager = self.module.context.module_manager
+        #
+        node = "admin.task_node"
+        #
+        # list
+        #
+        if action == "list":
+            result = []
+            #
+            if node is not None and scope is not None:
+                plugin_name, task_node_name = node.split(".", 1)
+                #
+                if plugin_name in module_manager.modules:
+                    plugin = module_manager.modules[plugin_name].module
+                    task_node = getattr(plugin, task_node_name, None)
+                    #
+                    if task_node is not None:
+                        if scope == "task":
+                            with task_node.lock:
+                                for state in task_node.global_task_state.values():
+                                    meta = state.get("meta", None)
+                                    if meta is not None:
+                                        meta = str(meta)
+                                    #
+                                    status = state.get("status", None)
+                                    if status == "stopped":
+                                        continue
+                                    #
+                                    result.append({
+                                        "task_id": state.get("task_id", None),
+                                        "requestor": state.get("requestor", None),
+                                        "runner": state.get("runner", None),
+                                        "status": status,
+                                        "meta": meta,
+                                        "node": node,
+                                    })
+            #
+            return {
+                "total": len(result),
+                "rows": result,
+            }
+        # #
+        # # refresh
+        # #
+        # if action == "refresh":
+        #     if node is None or scope is None:
+        #         return {
+        #             "ok": False,
+        #             "error": "node or scope not set",
+        #         }
+        #     #
+        #     plugin_name, task_node_name = node.split(".", 1)
+        #     #
+        #     if plugin_name not in module_manager.modules:
+        #         return {
+        #             "ok": False,
+        #             "error": "unknown plugin",
+        #         }
+        #     #
+        #     plugin = module_manager.modules[plugin_name].module
+        #     task_node = getattr(plugin, task_node_name, None)
+        #     #
+        #     if task_node is None:
+        #         return {
+        #             "ok": False,
+        #             "error": "unknown node",
+        #         }
+        #     #
+        #     if scope == "pool":
+        #         task_node.query_pool_state()
+        #     elif scope == "task":
+        #         task_node.query_task_state()
+        #     else:
+        #         return {
+        #             "ok": False,
+        #             "error": "unknown scope",
+        #         }
+        #     #
+        #     return {
+        #         "ok": True,
+        #     }
+        #
+        # stop
+        #
+        if action == "stop":
+            if node is None or scope is None:
+                return {
+                    "ok": False,
+                    "error": "node or scope not set",
+                }
+            #
+            plugin_name, task_node_name = node.split(".", 1)
+            #
+            if plugin_name not in module_manager.modules:
+                return {
+                    "ok": False,
+                    "error": "unknown plugin",
+                }
+            #
+            plugin = module_manager.modules[plugin_name].module
+            task_node = getattr(plugin, task_node_name, None)
+            #
+            if task_node is None:
+                return {
+                    "ok": False,
+                    "error": "unknown node",
+                }
+            #
+            task_node.stop_task(scope)
+            #
+            return {
+                "ok": True,
+            }
+        #
+        return {
+            "ok": False,
+            "error": "unknown action",
+        }
+
+
+class API(api_tools.APIBase):  # pylint: disable=R0903
+    """ API """
+
+    url_params = [
+        "<string:mode>",
+    ]
+
+    mode_handlers = {
+        'administration': AdminAPI,
+    }
---
+++ static/js/tasks.js
@@ -0,0 +1,84 @@
+$(".refresh-table-button").click(function() {
+  $($(this).data("target")).bootstrapTable("refresh", {});
+});
+
+
+$("#btn-start-task").click(function() {
+  // var target = $(this).data("target");
+  // //
+  // axios.get(tasks_api_url, {params: {action: "refresh", scope: "task"}})
+  //   .then(function (response) {
+  //     if (response.data.ok) {
+  //       showNotify("SUCCESS", "Refresh request completed");
+  //       $(target).bootstrapTable("refresh", {});
+  //     } else {
+  //       showNotify("ERROR", response.data.error);
+  //     }
+  //   })
+  //   .catch(function (error) {
+  //     showNotify("ERROR", "Error during refresh request");
+  //     console.log(error);
+  //   });
+});
+
+
+$("#modal-task-logs").on("show.bs.modal", function (e) {
+  $("#modal-logs-task").text(logs_row.task_id);
+  $("#input-logs").val("");
+  //
+  window.socket.emit("task_logs_subscribe", {"tasknode_task": "id:" + logs_row.task_id});
+});
+
+
+$("#modal-task-logs").on("hide.bs.modal", function (e) {
+  $("#modal-logs-task").text("");
+  $("#input-logs").val("");
+  //
+  window.socket.emit("task_logs_unsubscribe", {"tasknode_task": "id:" + logs_row.task_id});
+});
+
+
+$(document).on("vue_init", () => {
+  window.socket.on("log_data", (data) => {
+    data.forEach((item) => {
+      $("#input-logs").val(
+        $("#input-logs").val() + item.line + "\n"
+      );
+    });
+  });
+});
+
+
+function actions_formatter(value, row, index) {
+  return [
+    '<a class="task-show-logs" href="javascript:void(0)" title="Show logs">',
+    '  <i class="fa fa-file-text" style="color: #858796"></i>',
+    '</a>',
+    '&nbsp;',
+    '<a class="task-stop" href="javascript:void(0)" title="Stop">',
+    '  <i class="fa fa-stop"></i>',
+    '</a>',
+  ].join('')
+}
+
+
+window.actions_events = {
+  "click .task-stop": function (e, value, row, index) {
+    axios.get(tasks_api_url, {params: {action: "stop", scope: row.task_id}})
+      .then(function (response) {
+        if (response.data.ok) {
+          showNotify("SUCCESS", "Stop requested");
+        } else {
+          showNotify("ERROR", response.data.error);
+        }
+      })
+      .catch(function (error) {
+        showNotify("ERROR", "Error during stop request");
+        console.log(error);
+      });
+  },
+  "click .task-show-logs": function (e, value, row, index) {
+    logs_row = row;
+    $("#modal-task-logs").modal("show");
+  }
+}
