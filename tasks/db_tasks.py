efbece8bc099cb8b4ba28cbc1d2f77fccf8c02d9
Ivan Krakhmaliuk <Ivan_Krakhmaliuk@epam.com>
2025-03-11 17:05:57 +0200

    Tasks: prj diff

--- tasks/db_tasks.py
+++ tasks/db_tasks.py
@@ -62,7 +62,7 @@ def create_tables():
         log.info("Exiting (duration = %s)", end_ts - start_ts)
 
 
-def propose_migrations():
+def propose_migrations():  # pylint: disable=R0914
     """ Task """
     #
     with make_logger() as log:
@@ -72,21 +72,68 @@ def propose_migrations():
         try:
             from tools import db  # pylint: disable=C0415,E0401
             #
-            log.info("Getting shared metadata")
-            shared_metadata = db.get_shared_metadata()
-            #
             from alembic.migration import MigrationContext  # pylint: disable=C0415,E0401
             from alembic.autogenerate import compare_metadata  # pylint: disable=C0415,E0401
             #
+            # ---
+            #
+            log.info("Getting shared metadata")
+            shared_metadata = db.get_shared_metadata()
+            #
             log.info("Comparing shared metadata")
             with db.get_session(None) as shared_db:
+                def _non_auth_name(name, type_, parent_names):  # pylint: disable=W0613
+                    return "auth_core" not in name
+                #
                 migration_ctx = MigrationContext.configure(
                     connection=shared_db.connection(),
+                    opts={
+                        "include_name": _non_auth_name,
+                    },
                 )
                 #
-                db_diff = compare_metadata(migration_ctx, shared_metadata)
+                db_diffs = compare_metadata(migration_ctx, shared_metadata)
                 #
-                log.info("DB diff: %s", db_diff)
+                for db_diff in db_diffs:
+                    log.info("- Shared DB diff: %s", db_diff)
+            #
+            # ---
+            #
+            log.info("Getting project metadata")
+            tenant_metadata = db.get_tenant_specific_metadata()
+            #
+            log.info("Getting project list")
+            project_list = context.rpc_manager.timeout(120).project_list(
+                filter_={"create_success": True},
+            )
+            #
+            if project_list:
+                project = project_list[0]
+                #
+                log.info("Comparing project metadata: %s", project)
+                with db.get_session(project["id"]) as tenant_db:
+                    from tools import project_constants as pc  # pylint: disable=E0401,C0415
+                    project_schema = pc["PROJECT_SCHEMA_TEMPLATE"].format(project["id"])
+                    #
+                    def _project_schema_name(name, type_, parent_names):  # pylint: disable=W0613
+                        if type_ == "schema":
+                            return name == project_schema
+                        #
+                        return True
+                    #
+                    migration_ctx = MigrationContext.configure(
+                        connection=tenant_db.connection(),
+                        opts={
+                            "include_schemas": True,
+                            "include_name": _project_schema_name,
+                        },
+                    )
+                    #
+                    db_diffs = compare_metadata(migration_ctx, tenant_metadata)
+                    #
+                    for db_diff in db_diffs:
+                        log.info("- Project DB diff: %s", db_diff)
+            #
         except:  # pylint: disable=W0702
             log.exception("Got exception, stopping")
         #
